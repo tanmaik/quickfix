d12cb592b2198af22a39b207c55a56e59695b903
Jay Lalwani <lalwanijay2005@gmail.com>
2022-04-16 21:20:20 -0400

    920 set up people's list

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Button from "./components/Button.js";
-
+import Calendar from "./components/calendar";
+import UserCards from "./components/idcard";
+import Test from "./components/test";
 export default function Home() {
   return (
     <div>
-      <Button buttonTitle="hi tanmai"></Button>
+      <UserCards />
+      <Test />
     </div>
   );
 }
